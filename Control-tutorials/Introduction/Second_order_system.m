86736583bba4187e49fec2805c7d8d022b14e8ac
HanyuWu <wuh199410@gmail.com>
2020-02-07 18:20:12 -0500

    Add overdamped, undamped , critical damped situation.

--- Control-tutorials/Introduction/Second_order_system.m
+++ Control-tutorials/Introduction/Second_order_system.m
@@ -1,4 +1,4 @@
-% Second-Order Systems 16520020710111200020
+% Second-Order Systems  16520020710111200020
 %% Underdamped Systems
 
 k_dc = 1;     % DC gain
@@ -17,3 +17,43 @@ axis([-3 1 -15 15])
 step(G1)
 axis([0 3 0 2])
 
+%% Overdamped System
+
+zeta = 2.0;
+
+G2 = k_dc*w_n^2/(s^2 + 2*zeta*w_n*s + w_n^2);
+
+pzmap(G2)
+axis([-20 1 -1 1])
+
+step(G2)
+axis([0 1.5 0 1.5])
+
+%% Critically-Damped Systems
+
+zeta = 1;
+
+G3 = k_dc*w_n^2/(s^2 + 2*zeta*w_n*s + w_n^2);
+
+pzmap(G3)
+axis([-11 1 -1 1])
+
+step(G3)
+axis([0 1.5 0 1.5])
+
+%% Undamped System
+
+zeta = 0;
+
+G4 = k_dc*w_n^2/(s^2 + 2*zeta*w_n*s + w_n^2);
+
+pzmap(G4)
+axis([-1 1 -15 15])
+
+step(G4)
+axis([0 5 -0.5 2.5])
+
+%% Bode Plot (overall)
+
+bode(G1,G2,G3,G4)
+legend('underdamped: zeta < 1','overdamped: zeta > 1','critically-damped: zeta = 1','undamped: zeta = 0')
